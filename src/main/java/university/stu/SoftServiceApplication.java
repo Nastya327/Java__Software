8eb4cc5898650bec2ecb9b477fed8bf48dc7cbd3
PashaBalandin <pashabalandin1200@gmail.com>
2020-12-19 12:21:14 +0200

    first coomit

---
+++ src/main/java/university/stu/SoftServiceApplication.java
@@ -0,0 +1,13 @@
+package university.stu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/university/stu/SoftServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package university.stu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
